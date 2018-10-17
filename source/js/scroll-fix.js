820b2334c903b7a02207c9e5a8d39f053095a5a8
Only <lifedamara@163.com>
2018-10-17 14:39:32 +0800

    add post toc and side fixed

---
+++ source/js/scroll-fix.js
@@ -0,0 +1,24 @@
+document.onreadystatechange = function () {
+  if (document.readyState === 'complete') {
+    initScrollFix()
+  }
+}
+
+function initScrollFix() {
+  var oFix = document.querySelector('.scroll-fix')
+  if (oFix) {
+    var top = oFix.offsetTop,
+        left = oFix.offsetLeft
+    document.addEventListener('scroll', watch)
+  }
+  
+  function watch() {
+    if (document.documentElement.scrollTop >= top) {
+      oFix.style.position = 'fixed'
+      oFix.style.top = 0
+      oFix.style.left = left
+    } else {
+      oFix.style.position = 'static'
+    }
+  }
+}
